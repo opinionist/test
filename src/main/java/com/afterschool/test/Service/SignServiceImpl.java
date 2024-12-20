7776001b04fa950218b6d64152dc4ecee8d74c9e
minsung gwak <131960511+opinionist@users.noreply.github.com>
2024-12-20 10:59:28 +0900

    spring security 책으로 공부하는 것 11월 20일 3교시

--- src/main/java/com/afterschool/test/Service/SignServiceImpl.java
+++ src/main/java/com/afterschool/test/Service/SignServiceImpl.java
@@ -1,7 +1,9 @@
 package com.afterschool.test.Service;
 
+import com.afterschool.test.Entity.User;
 import com.afterschool.test.Entity.dto.SignUpResultDto;
 import com.afterschool.test.Entity.dto.SigninResultDto;
+import com.afterschool.test.jwt.CommonResponse;
 import com.afterschool.test.jwt.JwtTokenProvider;
 import com.afterschool.test.repository.UserRepo;
 import org.slf4j.Logger;
@@ -9,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class SignServiceImpl implements SignService{
@@ -27,11 +31,49 @@ public class SignServiceImpl implements SignService{
 
     @Override
     public SignUpResultDto signUp(String id, String password, String name, String role){
-        return null;
+        logger.info("[getSignUpResult] 회원 가입 정보 전달");
+        User user;
+        if(role.equalsIgnoreCase("admin")){
+            user = User.builder()
+                    .uid(id)
+                    .name(name)
+                    .password(passwordEncoder.encode(password))
+                    .roles(Collections.singletonList("ROLE_ADMIN"))
+                    .build();
+        }
+        else {
+            user = User.builder()
+                    .uid(id)
+                    .name(name)
+                    .password(passwordEncoder.encode(password))
+                    .roles(Collections.singletonList("ROLE_USER"))
+                    .build();
+        }
+
+        User savedUser = userRepo.save(user);
+        SignUpResultDto signUpResultDto = new SignUpResultDto();
+
+        logger.info("[getSignResult] userEntity 값이 들어왔는지 확인 후 결과값 주입");
+        if(!savedUser.getName().isEmpty()){
+            logger.info("[getSignResult] userEntity 정상 처리 완료");
+
+        }
     }
 
     @Override
     public SigninResultDto signIn(String id, String password) throws RuntimeException {
         return null;
+    }
+
+    private void setSuccessResult(SignUpResultDto result){
+        result.setSuccess(true);
+        result.setCode(CommonResponse.SUCCESS.getCode());
+        result.setMsg(CommonResponse.SUCCESS.getMsg());
+    }
+
+    private void setFailResult(SignUpResultDto result){
+        result.setSuccess(false);
+        result.setCode(CommonResponse.FAIL.getCode());
+        result.setMsg(CommonResponse.FAIL.getMsg());
     }
 }
